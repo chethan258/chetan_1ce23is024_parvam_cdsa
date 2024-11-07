8a654853a07f229c2dd13abfa2ec3ef1d503a5f2
chethan258 <147502009+chethan258@users.noreply.github.com>
2024-11-07 16:02:20 +0530

    Add files via upload

---
+++ fibonacce.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+long fibo(int);
+int main()
+{
+    int n=0;
+    scanf("%d",&n);
+     printf("%ld",fibo(n));
+    return 0;
+}
+
+long fibo(int no)
+{
+    if(no<=0)return 0;
+    if(no<=1)return 1;
+    return fibo(no-1)+fibo(no-2);
+}
